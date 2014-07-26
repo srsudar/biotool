2aae554ba60b9ce64161428fbf637fa045c4a778
Sam Sudar <sudar.sam@gmail.com>
2014-07-26 14:54:49 -0500

    context menus showing up. Click isn't doing anything.

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -5,3 +5,38 @@ chrome.runtime.onInstalled.addListener(function (details) {
 });
 
 console.log('\'Allo \'Allo! Event Page');
+
+// This function will be replaced. For now we'll just note that we're calling
+// the function correctly with a log statement.
+function genericOnClick(info, tab) {
+    console.log('item ' + info.menuItemId + ' was clicked');
+    console.log('info: ' + JSON.stringify(info));
+    console.log('tab: ' + JSON.stringify(tab));
+}
+
+// We're going to create some context menu items. We need three such menus:
+// paste reverse
+// paste complement
+// paste reverse complement
+// This is following the example set forth in:
+// https://developer.chrome.com/extensions/samples#search:
+var parentItem = chrome.contextMenus.create(
+        {'title': 'BioTool test parent item'});
+var pasteReverse = chrome.contextMenus.create(
+    {
+        'title': 'Paste Reverse',
+        'parentId': parentItem,
+        'onclick': genericOnClick
+    });
+var pasteComplement = chrome.contextMenus.create(
+    {
+        'title': 'Paste Complement',
+        'parentId': parentItem,
+        'onclick': genericOnClick
+    });
+var pasteReverseComplement = chrome.contextMenus.create(
+    {
+        'title': 'Paste Reverse Complement',
+        'parentId': parentItem,
+        'onclick': genericOnClick
+    });
