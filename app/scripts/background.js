55981201c36ce60fb702beb740fd2a96525531c8
Sam Sudar <sudar.sam@gmail.com>
2014-07-26 16:27:06 -0500

    got working on editables with event pages. Some gotchas going on there.

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,48 +1,71 @@
 'use strict';
 
+/**
+ * The function that will handle our context menu clicks.
+ */
+function onClickHandler(info, tab) {
+    if (info.menuItemId === 'pasteReverse') {
+        console.log('clicked paste reverse');
+    } else if (info.menuItemid === 'pasteComplement') {
+        console.log('clicked paste complement');
+    } else if (info.menuItemId === 'pasteReverseComplement') {
+        console.log('clicked paste reverse complement');
+    } else {
+        console.log('unrecognized context menu id: ' + info.menuItemId);
+    }
+}
+
+chrome.contextMenus.onClicked.addListener(onClickHandler);
+
+// we're going to set up the context menu tree at install time.
 chrome.runtime.onInstalled.addListener(function (details) {
     console.log('previousVersion', details.previousVersion);
+
+    // We're going to create some context menu items. We need three such menus:
+    // paste reverse
+    // paste complement
+    // paste reverse complement
+    // This is following the example set forth in:
+    // https://developer.chrome.com/extensions/samples#search:
+    var parentItem = chrome.contextMenus.create(
+        {
+            'title': 'BioTool test parent item',
+            'id': 'parentItem',
+            'contexts': ['editable']
+        });
+    var pasteReverse = chrome.contextMenus.create(
+        {
+            'title': 'Paste Reverse',
+            'id': 'pasteReverse',
+            'parentId': parentItem,
+            'contexts': ['editable']
+        },
+        function() {
+            if (chrome.extension.lastError) {
+                console.log(
+                    'Got unexpected error in pasteReverse item: ' +
+                    chrome.extension.lastError.message);
+            }
+            console.log('calling paste');
+            //document.getElementsByTagName('textarea')[0].focus();
+            //document.execCommand('paste');
+            //alert('hello');
+        });
+    var pasteComplement = chrome.contextMenus.create(
+        {
+            'title': 'Paste Complement',
+            'id': 'pasteComplement',
+            'parentId': parentItem,
+            'contexts': ['editable']
+        });
+    var pasteReverseComplement = chrome.contextMenus.create(
+        {
+            'title': 'Paste Reverse Complement',
+            'id': 'pasteReverseComplement',
+            'parentId': parentItem,
+            'contexts': ['editable']
+        });
 });
 
 console.log('\'Allo \'Allo! Event Page');
 
-// This function will be replaced. For now we'll just note that we're calling
-// the function correctly with a log statement.
-function genericOnClick(info, tab) {
-    console.log('item ' + info.menuItemId + ' was clicked');
-    console.log('info: ' + JSON.stringify(info));
-    console.log('tab: ' + JSON.stringify(tab));
-}
-
-// We're going to create some context menu items. We need three such menus:
-// paste reverse
-// paste complement
-// paste reverse complement
-// This is following the example set forth in:
-// https://developer.chrome.com/extensions/samples#search:
-var parentItem = chrome.contextMenus.create(
-    {
-        'title': 'BioTool test parent item',
-        'contexts': ['editable']
-    });
-var pasteReverse = chrome.contextMenus.create(
-    {
-        'title': 'Paste Reverse',
-        'parentId': parentItem,
-        'onclick': genericOnClick,
-        'contexts': ['editable']
-    });
-var pasteComplement = chrome.contextMenus.create(
-    {
-        'title': 'Paste Complement',
-        'parentId': parentItem,
-        'onclick': genericOnClick,
-        'contexts': ['editable']
-    });
-var pasteReverseComplement = chrome.contextMenus.create(
-    {
-        'title': 'Paste Reverse Complement',
-        'parentId': parentItem,
-        'onclick': genericOnClick,
-        'contexts': ['editable']
-    });
