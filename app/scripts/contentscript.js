c2b56e2f9b91ffefdda65ed1694db20e7a8758ca
Sam Sudar <sudar.sam@gmail.com>
2014-07-26 18:14:26 -0500

    updated content script to insert at point of selection rather than at the end or replacing text.

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,19 +1,36 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Content script');
-
-function pasteContent(toBePasted) {
-    document.activeElement.value += toBePasted;
+/**
+ * Insert the text at the cursor into the active element. Note that we're
+ * intentionally not appending or simply replacing the value of the editable
+ * field, as we want to allow normal pasting conventions. If you paste at the
+ * beginning, you shouldn't see all your text be replaced.
+ * Taken from:
+ * http://stackoverflow.com/questions/7404366/how-do-i-insert-some-text-where-the-cursor-is
+ */
+function insertTextAtCursor(text) {
+    var el = document.activeElement;
+    var val = el.value;
+    var endIndex;
+    var range;
+    var doc = el.ownerDocument;
+    if (typeof el.selectionStart === 'number' &&
+        typeof el.selectionEnd === 'number') {
+        endIndex = el.selectionEnd;
+        el.value = val.slice(0, endIndex) + text + val.slice(endIndex);
+        el.selectionStart = el.selectionEnd = endIndex + text.length;
+    } else if (doc.selection !== 'undefined' && doc.selection.createRange) {
+        el.focus();
+        range = doc.selection.createRange();
+        range.collapse(false);
+        range.text = text;
+        range.select();
+    }
 }
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    console.log(sender.tab ?
-        'from a content script: ' + sender.tab.url :
-        'from the extension');
     if (request.data) {
-        console.log('trying to paste: ' + request.data);
-        pasteContent(request.data);
+        insertTextAtCursor(request.data);
     }
-    console.log('wrapping up response');
 });
 
