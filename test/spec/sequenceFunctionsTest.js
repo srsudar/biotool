f754ba0cb8bb50a251e021e774d675a5b624c65f
Sam Sudar <sudar.sam@gmail.com>
2014-07-26 22:05:42 -0500

    all tests passing again.

--- test/spec/sequenceFunctionsTest.js
+++ test/spec/sequenceFunctionsTest.js
@@ -291,86 +291,86 @@
 
     // Oct 4 with acceptable non-base characters 
     var oct4Dirty =
-        '1 gaggtgaaac cgtccctagg tgagccgtct ttccaccagg cccccggctc ggggtgccca' +
-       '61 ccttccccat ggctggacac ctggcttcag acttcgcctt ctcaccccca ccaggtgggg' +
-      '121 gtgatgggtc agcagggctg gagccgggct gggtggatcc tcgaacctgg ctaagcttcc' +
-      '181 aagggcctcc aggtgggcct ggaatcggac caggctcaga ggtattgggg atctccccat' +
-      '241 gtccgcccgc atacgagttc tgcggaggga tggcatactg tggacctcag gttggactgg' +
-      '301 gcctagtccc ccaagttggc gtggagactt tgcagcctga gggccaggca ggagcacgag' +
-      '361 tggaaagcaa ctcagaggga acctcctctg agccctgtgc cgaccgcccc aatgccgtga' +
-      '421 agttggagaa ggtggaacca actcccgagg aggtaagtga agggacttgg ctgggctggc' +
-      '481 agaggcagca gtgaagggaa ttgggaacat gtagggtagc caccctgcct gccaaaggtg' +
-      '541 gtgatggctg ccgggcctcc tgagaagcac gacgcagtgt ggactagaac ccagaattgc' +
-      '601 aagaatcaga aaccggcctg gattgtttcg gcctggccct tgtcatgtag gtcacctagg' +
-      '661 cctggcctgt gtcccgacac ttgcttcatg ccatcactgt ctgtacacca gtgatgcgtg' +
-      '721 aaaatcagcc cccccccaaa aaaaaaaaca tatcagcccc tctggggact tggatcacag' +
-      '781 tcggacccag gaacttggcc ttaaggttag gcatggctgg gggggtaaaa aatggtgctt' +
-      '841 atcctggagt tattgttact gaagaggttg ggtgtgactg gctgctgata ggagctcttg' +
-      '901 tttgggccat gtgtggagta gggctcacct tcagtcaagt ttacggcctg tctactttag' +
-      '961 cctcagactc catgagtcac ctttacacga gcagaccctt gtagtgcctg aggtgcagat' +
-     '1021 ctgatcgatt tcagcctttc tacctttcct tgtaaacaag aaagggacac ccttgggtag' +
-     '1081 gggagtttta tctccaggcc atcttaagat cattctgtga gtgcacgggc cttgcttagt' +
-     '1141 gtctgatggc ctacagccag cactctggag caagtgtaag caattagcct taagaacaag' +
-     '1201 gtgcgagtgg ataccgatgc ccgccgggag ttccgacagc ttagcgattg ttgtagcagg' +
-     '1261 agtcccctcc ctaagtgcca gtttctgtgt tatctcaggt cctgtatgcc gccgggagtc' +
-     '1321 ccctaggaag gcattaatag tttatctcac atcttaaatg gcccttaatg aagcaagaga' +
-     '1381 tttgaacctt agttaagcta atcccaaatc ctcaaaatag gatttagaaa agccaaagac' +
-     '1441 actgctgagg gcgattacaa gttttggtct tttgaggagc agttggagat gaaagtctgt' +
-     '1501 ctgaagccga gagaatcctt ttccattgaa atggcattga ggtgtgcctc actggctgct' +
-     '1561 gcttctgtct gtgccctggg ttggccagcc tttgtggagc acctcagccc tccatcctgg' +
-     '1621 acctttgctc caacaacctg ctcctcttcc gccctcaagg ctgacttgca tctccccaga' +
-     '1681 tgactgcctc catttctgtc ttctgttaga gacagaaaag cctgagaaac cgacagccat' +
-     '1741 tttggggggg gggggtccgg ttcacacgct gcaacttaga aagcacactc aactggccat' +
-     '1801 ctgttatacc ctccccacct ggtcccaacc atcactgtgt actactgaga agaaggcagc' +
-     '1861 cttagccaca ccctcgagtg cccctgccgt tctattgctc atacatcgat tgatatccct' +
-     '1921 gtttcaactt tgaaaaaaaa aaattttttt ttttttgtgg tgtgtgcatg cctgctactg' +
-     '1981 tacacctgtg ggcgtcagag gtggtcctct gcaccctccg gccagtaccg catccagggt' +
-     '2041 gagtcagatg atttcctgtg gtttgggcct caaggcttct cacctccaga ggcttctagc' +
-     '2101 ctgctgcctt gctttctctg tcgcactcta gtacagcagg agttttcttc gcactccgga' +
-     '2161 gtgttgtcag ctcctggggc atggacattt ggctacttag agtgtgctgt gtaggttttc' +
-     '2221 atttagagct gaacagaggg atggatctta ttaccccagc ccttgagaca ctgaggcagg' +
-     '2281 agagcttcct agtgagtccc tgtttcaata tcttcactaa tactgtgtca tactttggga' +
-     '2341 ctttctttct tcctttcttt cttttgattt tttttttttt tatatgagta cagtgtacct' +
-     '2401 gtcttcagac acacaccaga agagggcatc agatcctact acagaaggtt gtgagccacc' +
-     '2461 ttgtggttgc tgggaattga actccggatc tccggaagag aagtccgtat accaacttct' +
-     '2521 gtattagtca gggttctcta gagtcacaga acttatggac agtctctaga tagtaaagga' +
-     '2581 atttattgat gacttacagt cggcagccca attcccaaca atggttcagt cgcagctgtg' +
-     '2641 aatggaagtc caaggatcta gcagttactt agtctcacgc agcaagcagg cgaaggagca' +
-     '2701 agagctagag cttaactgct gagccatgtg tttcttgagt aaagggatta catgctcgtt' +
-     '2761 cgtctggtca attctgcagc cttaaaactt cttcagaata gggtgacatt ttgtcctcag' +
-     '2821 tggggcggtt ttgagtaatc tgtgagcaga taggaacttg ctggggtact gcacagaact' +
-     '2881 ctgggtagtg tggtactgta gatggctagg ttctgggggg ggaaagagcc atctatgtca' +
-     '2941 cctaggaata gagtgaataa catttatata atcagaccag cccttgagga ggctgagatc' +
-     '3001 ttttcatggg gcaccctagg gtcacagtcc cagctggtgt gactctgaca agtctgcctt' +
-     '3061 tctcactaca gtcccaggac atgaaagccc tgcagaagga gctagaacag tttgccaagc' +
-     '3121 tgctgaagca gaagaggatc accttggggt acacccaggc cgacgtgggg ctcaccctgg' +
-     '3181 gcgttctctt tggtgggtct cccccagcat gttctgatct cacggctctt aatgtaggcg' +
-     '3241 caagggggtg gggcatctta ggagctgctt ctccacaggt aagggaggat tagacgcttg' +
-     '3301 tagcttgaac tgtcagaggt gggggcttgg gctcccttct tgctgcctca ctcactctgt' +
-     '3361 ttgatcggcc tttcaggaaa ggtgttcagc cagaccacca tctgtcgctt cgaggccttg' +
-     '3421 cagctcagcc ttaagaacat gtgtaagctg cggcccctgc tggagaagtg ggtggaggaa' +
-     '3481 gccgacaaca atgagaacct tcaggaggtg aggagtggca ggatgtgtgc aatgtctgcc' +
-     '3541 aggcacagtc ccttctgctg cttccattcc tggcttgaaa ctcctccctc tccaaccgga' +
-     '3601 gctcgcagga gaagttctgt gtccttattc tgctgctatg aattggaatc cagagcctta' +
-     '3661 acatttgcta atcaatcagg ctctctcctt ctgagtcacc ctctgccccc accagcctga' +
-     '3721 caatggtccc tccccagaac cccgtctagt gctggtgaag gctcagacct aggtctacca' +
-     '3781 gccccttcca gagccccttt cagtaacccc tggctctggg gccacatcca gtcaatgctc' +
-     '3841 ccttagcaca atcccttagc ggtttgttct tcagtcccat ctcaaggtgg ggctgttgcc' +
-     '3901 aagtcaaata ctaaagttgc tcttgtcgcc cccatcttcc cctgcccaga tatgcaaatc' +
-     '3961 ggagaccctg gtgcaggccc ggaagagaaa gcgaactagc attgagaacc gtgtgaggtg' +
-     '4021 gagtctggag accatgtttc tgaagtgccc gaagccctcc ctacagcaga tcactcacat' +
-     '4081 cgccaatcag cttgggctag agaaggatgt gagtgccaag atcctgccct gtggtacctg' +
-     '4141 gatgtttccc tgttcccatt ccccaccccc cccacccccc cacccccacc gccgccaccg' +
-     '4201 ctgactgcag catcccagag cttatgatct gatgtccatc tctgtgccca tcctaggtgg' +
-     '4261 ttcgagtatg gttctgtaac cggcgccaga agggcaaaag atcaagtatt gagtattccc' +
-     '4321 aacgagaaga gtatgaggct acagggacac ctttcccagg gggggctgta tcctttcctc' +
-     '4381 tgcccccagg tccccacttt ggcaccccag gctatggaag cccccacttc accacactct' +
-     '4441 actcagtccc ttttcctgag ggcgaggcct ttccctctgt tcccgtcact gctctgggct' +
-     '4501 ctcccatgca ttcaaactga ggcaccagcc ctccctgggg atgctgtgag ccaaggcaag' +
-     '4561 ggaggtagac aagagaacct ggagctttgg ggttaaattc ttttactgag gagggattaa' +
-     '4621 aagcacaaca ggggtggggg gtgggatggg gaaagaagct cagtgatgct gttgatcagg' +
-     '4681 agcctggcct gtctgtcact catcattttg ttcttaaata aagactggga cacacagtag' +
-     '4741 atagct';
+        '1gaggtgaaaccgtccctaggtgagccgtctttccaccaggcccccggctcggggtgccca' +
+       '61ccttccccatggctggacacctggcttcagacttcgccttctcacccccaccaggtgggg' +
+      '121gtgatgggtcagcagggctggagccgggctgggtggatcctcgaacctggctaagcttcc' +
+      '181aagggcctccaggtgggcctggaatcggaccaggctcagaggtattggggatctccccat' +
+      '241gtccgcccgcatacgagttctgcggagggatggcatactgtggacctcaggttggactgg' +
+      '301gcctagtcccccaagttggcgtggagactttgcagcctgagggccaggcaggagcacgag' +
+      '361tggaaagcaactcagagggaacctcctctgagccctgtgccgaccgccccaatgccgtga' +
+      '421agttggagaaggtggaaccaactcccgaggaggtaagtgaagggacttggctgggctggc' +
+      '481agaggcagcagtgaagggaattgggaacatgtagggtagccaccctgcctgccaaaggtg' +
+      '541gtgatggctgccgggcctcctgagaagcacgacgcagtgtggactagaacccagaattgc' +
+      '601aagaatcagaaaccggcctggattgtttcggcctggcccttgtcatgtaggtcacctagg' +
+      '661cctggcctgtgtcccgacacttgcttcatgccatcactgtctgtacaccagtgatgcgtg' +
+      '721aaaatcagcccccccccaaaaaaaaaaacatatcagcccctctggggacttggatcacag' +
+      '781tcggacccaggaacttggccttaaggttaggcatggctgggggggtaaaaaatggtgctt' +
+      '841atcctggagttattgttactgaagaggttgggtgtgactggctgctgataggagctcttg' +
+      '901tttgggccatgtgtggagtagggctcaccttcagtcaagtttacggcctgtctactttag' +
+      '961cctcagactccatgagtcacctttacacgagcagacccttgtagtgcctgaggtgcagat' +
+     '1021ctgatcgatttcagcctttctacctttccttgtaaacaagaaagggacacccttgggtag' +
+     '1081gggagttttatctccaggccatcttaagatcattctgtgagtgcacgggccttgcttagt' +
+     '1141gtctgatggcctacagccagcactctggagcaagtgtaagcaattagccttaagaacaag' +
+     '1201gtgcgagtggataccgatgcccgccgggagttccgacagcttagcgattgttgtagcagg' +
+     '1261agtcccctccctaagtgccagtttctgtgttatctcaggtcctgtatgccgccgggagtc' +
+     '1321ccctaggaaggcattaatagtttatctcacatcttaaatggcccttaatgaagcaagaga' +
+     '1381tttgaaccttagttaagctaatcccaaatcctcaaaataggatttagaaaagccaaagac' +
+     '1441actgctgagggcgattacaagttttggtcttttgaggagcagttggagatgaaagtctgt' +
+     '1501ctgaagccgagagaatccttttccattgaaatggcattgaggtgtgcctcactggctgct' +
+     '1561gcttctgtctgtgccctgggttggccagcctttgtggagcacctcagccctccatcctgg' +
+     '1621acctttgctccaacaacctgctcctcttccgccctcaaggctgacttgcatctccccaga' +
+     '1681tgactgcctccatttctgtcttctgttagagacagaaaagcctgagaaaccgacagccat' +
+     '1741tttggggggggggggtccggttcacacgctgcaacttagaaagcacactcaactggccat' +
+     '1801ctgttataccctccccacctggtcccaaccatcactgtgtactactgagaagaaggcagc' +
+     '1861cttagccacaccctcgagtgcccctgccgttctattgctcatacatcgattgatatccct' +
+     '1921gtttcaactttgaaaaaaaaaaatttttttttttttgtggtgtgtgcatgcctgctactg' +
+     '1981tacacctgtgggcgtcagaggtggtcctctgcaccctccggccagtaccgcatccagggt' +
+     '2041gagtcagatgatttcctgtggtttgggcctcaaggcttctcacctccagaggcttctagc' +
+     '2101ctgctgccttgctttctctgtcgcactctagtacagcaggagttttcttcgcactccgga' +
+     '2161gtgttgtcagctcctggggcatggacatttggctacttagagtgtgctgtgtaggttttc' +
+     '2221atttagagctgaacagagggatggatcttattaccccagcccttgagacactgaggcagg' +
+     '2281agagcttcctagtgagtccctgtttcaatatcttcactaatactgtgtcatactttggga' +
+     '2341ctttctttcttcctttctttcttttgattttttttttttttatatgagtacagtgtacct' +
+     '2401gtcttcagacacacaccagaagagggcatcagatcctactacagaaggttgtgagccacc' +
+     '2461ttgtggttgctgggaattgaactccggatctccggaagagaagtccgtataccaacttct' +
+     '2521gtattagtcagggttctctagagtcacagaacttatggacagtctctagatagtaaagga' +
+     '2581atttattgatgacttacagtcggcagcccaattcccaacaatggttcagtcgcagctgtg' +
+     '2641aatggaagtccaaggatctagcagttacttagtctcacgcagcaagcaggcgaaggagca' +
+     '2701agagctagagcttaactgctgagccatgtgtttcttgagtaaagggattacatgctcgtt' +
+     '2761cgtctggtcaattctgcagccttaaaacttcttcagaatagggtgacattttgtcctcag' +
+     '2821tggggcggttttgagtaatctgtgagcagataggaacttgctggggtactgcacagaact' +
+     '2881ctgggtagtgtggtactgtagatggctaggttctggggggggaaagagccatctatgtca' +
+     '2941cctaggaatagagtgaataacatttatataatcagaccagcccttgaggaggctgagatc' +
+     '3001ttttcatggggcaccctagggtcacagtcccagctggtgtgactctgacaagtctgcctt' +
+     '3061tctcactacagtcccaggacatgaaagccctgcagaaggagctagaacagtttgccaagc' +
+     '3121tgctgaagcagaagaggatcaccttggggtacacccaggccgacgtggggctcaccctgg' +
+     '3181gcgttctctttggtgggtctcccccagcatgttctgatctcacggctcttaatgtaggcg' +
+     '3241caagggggtggggcatcttaggagctgcttctccacaggtaagggaggattagacgcttg' +
+     '3301tagcttgaactgtcagaggtgggggcttgggctcccttcttgctgcctcactcactctgt' +
+     '3361ttgatcggcctttcaggaaaggtgttcagccagaccaccatctgtcgcttcgaggccttg' +
+     '3421cagctcagccttaagaacatgtgtaagctgcggcccctgctggagaagtgggtggaggaa' +
+     '3481gccgacaacaatgagaaccttcaggaggtgaggagtggcaggatgtgtgcaatgtctgcc' +
+     '3541aggcacagtcccttctgctgcttccattcctggcttgaaactcctccctctccaaccgga' +
+     '3601gctcgcaggagaagttctgtgtccttattctgctgctatgaattggaatccagagcctta' +
+     '3661acatttgctaatcaatcaggctctctccttctgagtcaccctctgcccccaccagcctga' +
+     '3721caatggtccctccccagaaccccgtctagtgctggtgaaggctcagacctaggtctacca' +
+     '3781gccccttccagagcccctttcagtaacccctggctctggggccacatccagtcaatgctc' +
+     '3841ccttagcacaatcccttagcggtttgttcttcagtcccatctcaaggtggggctgttgcc' +
+     '3901aagtcaaatactaaagttgctcttgtcgcccccatcttcccctgcccagatatgcaaatc' +
+     '3961ggagaccctggtgcaggcccggaagagaaagcgaactagcattgagaaccgtgtgaggtg' +
+     '4021gagtctggagaccatgtttctgaagtgcccgaagccctccctacagcagatcactcacat' +
+     '4081cgccaatcagcttgggctagagaaggatgtgagtgccaagatcctgccctgtggtacctg' +
+     '4141gatgtttccctgttcccattccccaccccccccacccccccacccccaccgccgccaccg' +
+     '4201ctgactgcagcatcccagagcttatgatctgatgtccatctctgtgcccatcctaggtgg' +
+     '4261ttcgagtatggttctgtaaccggcgccagaagggcaaaagatcaagtattgagtattccc' +
+     '4321aacgagaagagtatgaggctacagggacacctttcccagggggggctgtatcctttcctc' +
+     '4381tgcccccaggtccccactttggcaccccaggctatggaagcccccacttcaccacactct' +
+     '4441actcagtcccttttcctgagggcgaggcctttccctctgttcccgtcactgctctgggct' +
+     '4501ctcccatgcattcaaactgaggcaccagccctccctggggatgctgtgagccaaggcaag' +
+     '4561ggaggtagacaagagaacctggagctttggggttaaattcttttactgaggagggattaa' +
+     '4621aagcacaacaggggtggggggtgggatggggaaagaagctcagtgatgctgttgatcagg' +
+     '4681agcctggcctgtctgtcactcatcattttgttcttaaataaagactgggacacacagtag' +
+     '4741atagct';
 
     var functions = window.sequenceFunctions;
 
